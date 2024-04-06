bb94cc4347c18d91294e7d0f17c884893eca745a
audrey cherop <audreycherop06@gmail.com>
2024-04-06 22:15:31 +0300

    index.js

--- main.js
+++ main.js
@@ -3,8 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
+  const likeButtons = document.querySelectorAll(".like");
 
+  likeButtons.forEach((likeButton) => {
+    likeButton.addEventListener("click", async (event) => {
+      const heart =likeButton.querySelector(".like-glyph");
 
+      errorModal.classList.add("hidden");
+
+      const isLiked = heart.innerText === FULL_HEART;
+
+      try {
+        await mimicServerCall();
+        
+        if (isLiked) {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove("activated-heart");
+        } else {
+          heart.innerText = FULL_HEART;
+          heart.classList.add("activated-heart");
+        }
+      } catch (error) {
+        modalMessage.innerText = error;
+        errorModal.classList.remove("hidden");
+
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      }
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
